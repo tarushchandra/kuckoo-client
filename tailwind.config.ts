2455fd930d9e42f294db6586e1aff607d1e058d9
Tarush Chandra <tarushchandra6@gmail.com>
2025-08-27 19:12:48 +0530

    fix: added pages layout provider

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,11 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./providers/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
