ba1d827062bf80093aae16c5783547742e3ad0bb
Tarush Chandra <tarushchandra6@gmail.com>
2025-09-07 03:23:25 +0530

    fix: updated env variables in .env.sample and env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -4,5 +4,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_BACKEND_SERVER_URL: string;
     NEXT_PUBLIC_WEBSOCKET_URL: string;
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
   }
 }
