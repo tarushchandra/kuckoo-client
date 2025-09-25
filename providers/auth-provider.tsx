b286401cae26711679858cbfe40672fb6496f24b
Tarush Chandra <tarushchandra6@gmail.com>
2025-09-25 14:07:01 +0530

    fix: fixed the loading screen issue which was coming in production on sign out due to the cookie domain was not correctly setuped

--- providers/auth-provider.tsx
+++ providers/auth-provider.tsx
@@ -1,23 +1,39 @@
 "use client";
 import { useAuth } from "@/hooks/auth";
 import { selectAuth } from "@/lib/redux/features/auth/authSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AppLoading from "@/components/ui/app-loading";
 
 interface AuthProps {
   children: React.ReactNode;
-  hasToken: boolean;
+  isTokenInitiallyPresent: boolean;
 }
 
-export const AuthProvider: React.FC<AuthProps> = ({ children, hasToken }) => {
-  const { data: auth, signInAction } = useAuth(selectAuth);
-  const { isUserAuthenticated } = auth;
+export const AuthProvider: React.FC<AuthProps> = ({
+  children,
+  isTokenInitiallyPresent,
+}) => {
+  const { signInAction } = useAuth(selectAuth);
+  const [isInitialSignInAttempted, setIsInitialSignInAttempted] =
+    useState(false);
+
+  const handleInitialSignIn = async () => {
+    try {
+      await signInAction();
+    } finally {
+      setIsInitialSignInAttempted(true);
+    }
+  };
 
   // handle initial sign in when access token or refresh token is present
   useEffect(() => {
-    if (hasToken) signInAction();
+    if (isTokenInitiallyPresent) handleInitialSignIn();
   }, []);
 
-  if (hasToken && !isUserAuthenticated) return <AppLoading />;
+  // show loading screen only when token is initially present and initial sign in is not yet attempted
+  if (isTokenInitiallyPresent && !isInitialSignInAttempted)
+    return <AppLoading />;
+
+  // render children when no token is present (for public routes) or when initial sign in is attempted (for private routes)
   return <>{children}</>;
 };
--- providers/layouts/root.tsx
+++ providers/layouts/root.tsx
@@ -3,7 +3,7 @@ import ProgressBarProvider from "../progress-bar-provider";
 import { ReactQueryProvider } from "../react-query-provider";
 import { ReduxProvider } from "../redux-provider";
 import { AuthProvider } from "../auth-provider";
-import { getTokensFromCookies } from "@/lib/actions/user";
+import { getTokensFromCookies } from "@/lib/actions/auth";
 import { Toaster } from "react-hot-toast";
 
 interface ProvidersProps {
@@ -16,14 +16,16 @@ export const RootLayoutProvider: React.FC<ProvidersProps> = async ({
   const { accessToken, refreshToken } = await getTokensFromCookies();
   const hasAccessToken = accessToken ? true : false;
   const hasRefreshToken = refreshToken ? true : false;
-  const hasToken = hasAccessToken || hasRefreshToken;
+  const isTokenInitiallyPresent = hasAccessToken || hasRefreshToken;
 
   return (
     <>
       <ProgressBarProvider>
         <ReactQueryProvider>
           <ReduxProvider>
-            <AuthProvider hasToken={hasToken}>{children}</AuthProvider>
+            <AuthProvider isTokenInitiallyPresent={isTokenInitiallyPresent}>
+              {children}
+            </AuthProvider>
           </ReduxProvider>
         </ReactQueryProvider>
       </ProgressBarProvider>
